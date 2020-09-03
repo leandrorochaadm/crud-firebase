c586a31b7570b10b3eaf4df8411bf7cd18328dca
leandrorochaadm <leandrorochaadm@gmail.com>
2020-09-02 21:28:58 -0400

    add e exibindo lista de Pedidos

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,9 +11,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        color: Colors.amber,
+    CollectionReference pedidos =
+        FirebaseFirestore.instance.collection('pedidos');
+
+    Future<void> addPedido() {
+      // Call the user's CollectionReference to add a new user
+      return pedidos
+          .add({
+            'endereco': 'end 21',
+            'formaPagamento': 'formaPagamento',
+            'idCliente': 'idCliente 21',
+            'idEntregador': 'idEntregador 21',
+            'status': 'status',
+            'troco': 50.0,
+          })
+          .then((value) => print("pedidos Added"))
+          .catchError((error) => print("Failed to add pedido: $error"));
+    }
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addPedido();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: pedidos.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Erro'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return new ListView(
+            children: snapshot.data.docs.map((DocumentSnapshot document) {
+              return new ListTile(
+                title: new Text(document.data()['endereco']),
+                subtitle: new Text(document.data()['idEntregador']),
+                onTap: () {
+                  //abrir detalhes
+                },
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:motodelivery/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
